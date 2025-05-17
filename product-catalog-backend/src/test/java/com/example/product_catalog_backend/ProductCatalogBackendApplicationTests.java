d710ab31aeb23d0183df9f74c6e39032b3a09868
SL-SWE-HUB <rhizome.fens_0i@icloud.com>
2025-05-17 00:09:58 -0500

    Initial commit
    Creation of file structure for frontend and backend (with spring)

---
+++ product-catalog-backend/src/test/java/com/example/product_catalog_backend/ProductCatalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.product_catalog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
