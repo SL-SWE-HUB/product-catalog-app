b9eff120ec8a165e5af1d8c3b5b6290e0c6898d4
SL-SWE-HUB <rhizome.fens_0i@icloud.com>
2025-05-17 22:24:23 -0500

    Creation of ProductController and Implementation of CRUD endpoints

---
+++ product-catalog-backend/src/main/java/com/example/product_catalog_backend/controller/ProductController.java
@@ -0,0 +1,62 @@
+package com.example.product_catalog_backend.controller;
+
+import com.example.product_catalog_backend.entity.Product;
+import com.example.product_catalog_backend.service.ProductService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public List<Product> getAllProducts(){
+        return this.productService.getAllProducts();
+    }
+
+    @PostMapping("/create")
+    public Product createProduct(@RequestBody Product product){
+        if(product == null){
+            return null;
+        }
+        return this.productService.createProduct(product);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductByid(@PathVariable Long id){
+        Optional<Product> searchResult = this.productService.getProductById(id);
+        if(searchResult.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(searchResult.get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails){
+        Optional<Product> searchResult = this.productService.getProductById(id);
+        if(searchResult.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Product updatedProduct = productService.updateProduct(id, productDetails);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/{id")
+    public void deleteProduct(@PathVariable Long id){
+        this.productService.deleteProduct(id);
+    }
+}
