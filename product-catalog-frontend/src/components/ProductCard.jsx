98959e96eebc689fd797d4e4eb0db8c4d5caff1f
SL-SWE-HUB <rhizome.fens_0i@icloud.com>
2025-05-21 19:06:24 -0500

    ProductCard component implementation

---
+++ product-catalog-frontend/src/components/ProductCard.jsx
@@ -0,0 +1,23 @@
+
+function ProductCard({product}){
+    if(!product){
+        return <div>Product data is missing.</div>;
+    }
+    const formattedPrice = product.price? `$${Number(product.price).toFixed(2)}` : `Price not available`;
+
+    return(
+        <div className="product-card">
+            {/* Image Placeholder */}
+            <div className="product-image-holder">
+                {/* <img/> */}
+            </div>
+            <div className="product-info">
+                <h3 className="product-name">{product.name | "Unnamed Product"}</h3>
+                <p className="product-description">{product.description | "No Description Available."}</p>
+                <p className="product-name">{formattedPrice}</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
