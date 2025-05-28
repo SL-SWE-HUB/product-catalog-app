3319196a858dd56c9864b9979979bc9fac55e8df
SL-SWE-HUB <rhizome.fens_0i@icloud.com>
2025-05-27 20:01:27 -0500

    Implementation return statement

--- product-catalog-frontend/src/App.jsx
+++ product-catalog-frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import './App.css'
+import ProductList from './components/ProductList'
 
 function App() {
 
   return (
-   <div className="App">
-      <header className="App-header">
-        <h1>Minimal E-commerce Product Catalog</h1>
-      </header>
-      <main>
-        {/* placeholder for: ProductList Component */}
-        <p>Loading Products...</p>
-      </main>
-   </div> 
-  )
+    <div className="App">
+        <header className="App-header">
+            <h1>Minimal E-commerce Product Catalog</h1>
+        </header>
+        <main>
+            <ProductList />
+            {/* placeholder for: ProductList Component */}
+            <p>Loading Products...</p>
+        </main>
+        <footer className="App-footer">
+            <p>© 2025 SL-SWE-HUB</p>
+        </footer>
+    </div> 
+  );
 }
 
-export default App
+export default App;
