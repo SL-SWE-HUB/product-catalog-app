e934fd1536054fc82bc0c4eea2cfd84c7aceeba8
SL-SWE-HUB <rhizome.fens_0i@icloud.com>
2025-06-15 17:03:56 -0500

    Updated loading products into a conditional statement and refactored css file

--- product-catalog-frontend/src/App.jsx
+++ product-catalog-frontend/src/App.jsx
@@ -3,15 +3,17 @@ import ProductList from './components/ProductList'
 
 function App() {
 
+  if(!ProductList)
+    return <p>Loading Products...</p>;
+  
   return (
     <div className="App">
         <header className="App-header">
-            <h1>Minimal E-commerce Product Catalog</h1>
+            {/*<h1>Minimal E-commerce Product Catalog</h1>*/}
         </header>
         <main>
             <ProductList />
             {/* placeholder for: ProductList Component */}
-            <p>Loading Products...</p>
         </main>
         <footer className="App-footer">
             <p>© 2025 SL-SWE-HUB</p>
